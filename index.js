d6ba2e25f3bfc1a65e0c5115607d5593b1c84e39
Ronit Khadka <ronitkhadka4@gmail.com>
2026-05-17 13:16:01 +0545

    image render

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import authRoutes from "./routes/authRoutes.js";
 import fileUpload from "express-fileupload";
 import cors from "cors";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 
 
 const app = express();
@@ -29,8 +32,12 @@ app.get("/", (req, res) => {
   return res.status(200).json("Welcome to the social networking app");
 }
 )
-app.use('/uploads', express.static('uploads'));
-app.use('/profileuploads', express.static('profileuploads'));
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/profileuploads', express.static(path.join(__dirname, 'profileuploads')));
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
