70fc11b082cd440a3c6f9f8efbf6598fbb4eca49
Ronit Khadka <ronitkhadka4@gmail.com>
2026-05-17 13:37:39 +0545

    new changes for live

--- src/app/mainApi.js
+++ src/app/mainApi.js
@@ -1,11 +1,13 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 
-export const BASE_URL = 'http://192.168.1.69:5000';
+// export const BASE_URL = 'http://192.168.1.69:5000';
+export const BASE_URL = 'https://mernmangsir.onrender.com';
 export const mainApi = createApi({
   reducerPath: 'mainApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://192.168.1.69:5000/api',
+    // baseUrl: 'http://192.168.1.69:5000/api',
+    baseUrl: 'https://mernmangsir.onrender.com/api',
     // Automatically attaches token to every request
     // Reads from Redux state → state.user.token
     prepareHeaders: (headers, { getState }) => {
